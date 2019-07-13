185bc17e170e8b4a7465d279a698acd29557e944
Justice <justicetcloney@gmail.com>
2019-07-13 15:16:40 -0700

    documented ideas, began sculpting

--- citadelStart.rb
+++ citadelStart.rb
@@ -4,11 +4,9 @@
 # Prerequisites: Perl, Linux, dialog
 
 # TODO 
-# check/initialize for config directory
-# dummy import settings function
-# Show menu
 ## openvpn install/setup/control
 settings = Hash.new(0)
+
 def parseSetting (line)
     arr = line.split('=')
     settings[arr[0]] = arr[1]
@@ -30,6 +28,42 @@ def citadelInitialize (nondefaultConfig)
         puts "New file generated"
     end
    
+end
+
+def installMenu ()
+    # todo:
+    # openvpn
+    # cloud9ide
+    # nextcloud
+    # bitwarden
+    # SteamCMD and games
+    # minecraft server, maybe amp?
+    # 
+end
+def manageMenu ()
+    # run openvpn script to deauth/make new keys
+    # change common cloud9 settings easily, port, working directory
+    # nextcloud ???
+    # bitwarden update, maybe? might need admin web login.
+    # update steamcmd, change directories
+    # manage minecraft servers to a light extent. Refer people to AMP for a better solution. option to install?
+    # view and change backup settings
+    # apt update && upgrade
+    # pi menu
+end
+def monitorMenu ()
+    # htop
+    # tempurature
+    # drive statuses
+    # network statuses
+    # ping tools
+end
+def citadelMenu ()
+    system "clear" || system "cls" #clear screen for menu
+    puts "Citadel Menu:"
+    puts "1:install"
+    puts "2:manage"
+    puts "3:monitor"
 end
 citadelInitialize('')
 
